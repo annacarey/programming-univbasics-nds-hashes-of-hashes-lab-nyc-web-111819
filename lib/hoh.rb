61bb8a0fa091806377be7cc2af4cebe7ac545146
declarative-blog-8005 <anna@annajcarey.com>
2019-10-31 21:36:10 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,10 +46,8 @@ earth = {
 #   sub_category: {
 #     label: "Phylum",
 #     sub_category: {
-#       label: "Class"
+#       label: "Class",
 #     }
-#   } sub_category: {
-  label: "Order"
 }
   
 }
